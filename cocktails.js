76685afa1f16aa76de6c5344032a9d0689ab945a
Fabio Cerqueira <43950280+Fabioclock@users.noreply.github.com>
2020-10-01 10:53:52 +0200

    Initial commit

---
+++ cocktails.js
@@ -0,0 +1,119 @@
+const cocktails = [
+    {
+        "id": "5c08b816-bbf3-4bda-a28d-1ce9c0ff8ad4",
+        "name": "mojito",
+        "type": "longdrink",
+        "ingredients": [
+            "menthe",
+            "citron vert",
+            "sucre",
+            "rhum",
+            "eau gazeuse",
+            "angostura"
+        ],
+        "tags": [
+            "classique",
+            "frais"
+        ]
+    },
+    {
+        "id": "b42e411b-f325-4283-b95f-17362dd04d64",
+        "name": "Gin Fizz",
+        "type": "longdrink",
+        "ingredients": [
+            "citron vert",
+            "sucre",
+            "Gin",
+            "eau gazeuse"
+        ],
+        "tags": [
+            "classique",
+            "frais"
+        ]
+    },
+    {
+        "id": "e82f88ac-928b-44f3-b58e-c504b420ef98",
+        "name": "Old fashioned",
+        "type": "shortdrink",
+        "ingredients": [
+            "bourbon",
+            "sucre",
+            "angostura",
+            "zeste d'orange"
+        ],
+        "tags": [
+            "classique",
+            "fort"
+        ]
+    },
+    {
+        "id": "b1682416-6010-4cfa-8dad-dc0aa3a4aa86",
+        "name": "Sweety",
+        "type": "shooter",
+        "ingredients": [
+            "soho",
+            "champagne"
+        ],
+        "tags": [
+            "doux",
+            "chic"
+        ]
+    },
+    {
+        "id": "b2791108-b9b5-48e4-95e6-9c8ce9e63e92",
+        "name": "Bougie d'anniversaire",
+        "type": "shooter",
+        "ingredients": [
+            "Sirop de fraise",
+            "Curaçao",
+            "Cointrau"
+        ],
+        "tags": [
+            "flambé",
+            "sucré"
+        ]
+    },
+    {
+        "id": "c0a8e818-3761-4bc0-86e4-9f9e2db3c87d",
+        "name": "Schtroumpf",
+        "type": "shooter",
+        "ingredients": [
+            "Sirop de pèche",
+            "Vodka",
+            "Curaçao"
+        ],
+        "tags": [
+            "sucré"
+        ]
+    },
+    {
+        "id": "94409f8f-f094-4dd1-a6d9-d5de2164afb8",
+        "name": "Madelaine",
+        "type": "shooter",
+        "ingredients": [
+            "Cointreau",
+            "Amaretto",
+            "Liqueur de café (optionel)",
+            "Jus d'ananas"
+        ],
+        "tags": [
+            "sucré"
+        ]
+    },
+    {
+        "id": "92491c26-0bf4-4522-8041-dbc757cb2cf1",
+        "name": "Loft",
+        "type": "shooter",
+        "ingredients": [
+            "Baileys",
+            "Malibu",
+            "Sirop de caramel"
+        ],
+        "tags": [
+            "sucré"
+        ]
+    },
+​
+];
+
+module.exports = cocktails;
